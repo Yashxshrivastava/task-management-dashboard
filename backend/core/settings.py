439d9ad44fc5c56fa1e1a106f922e7b820608b24
Yash Kumar Shrivastava <yashshrivastava2001@gmail.com>
2026-04-21 00:28:19 +0530

    Add WhiteNoise to serve static files in production

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',#cors middleware
     'django.middleware.common.CommonMiddleware',
@@ -138,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 """
