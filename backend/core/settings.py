1f559fee3ec3cea8870b4fc81dadd967eea6b621
Yash Kumar Shrivastava <yashshrivastava2001@gmail.com>
2026-04-21 00:20:20 +0530

    Add Vercel URL to CORS allowed origins

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,4 +146,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 """
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://task-management-dashboard-teal.vercel.app",  # Your live Vercel frontend!
+]
